350aece0c5a5d27daf70c45892123b0540462070
OHKE 2024 <OHKE 2024@D318.lao>
2025-04-14 09:44:29 +0300

    Perustietotyypit.js tehty

---
+++ Perustietotyypit.js
@@ -0,0 +1,15 @@
+let merkkijono="Teksti";
+let numero=123;
+let totuusarvo=true;
+let tyhjä=null;
+let eiMaaritetty;
+let symboli=Symbol("symboli");
+let isoLuku=123456789012345678901234567890n;
+
+console.log(typeof merkkijono);
+console.log(typeof numero);
+console.log(typeof totuusarvo);
+console.log(typeof tyhjä);
+console.log(typeof eiMaaritetty);
+console.log(typeof symboli);
+console.log(typeof isoLuku);
